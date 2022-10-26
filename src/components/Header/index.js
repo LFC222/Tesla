6b75ef74dde100c7a980ee3d5fb2d02860099350
Lucas FC <lucasfernandes_222@hotmail.com>
2022-10-26 19:28:31 -0300

    Menu's

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import 
+{
+  Menu,
+  Container,
+  BurgerNav,
+  RightMenu,
+  CustomMenu,
+  CustomClose,
+  CloseWrapper,
+} from './styles'
+
 
 function Header() {
+
+  const [burgerStatus, setBurgerStatus] = useState(false);
+
   return (
-    <div>
-        Header
-    </div>
+    <Container>
+        <a href='/'>
+          <img src='/images/logo.svg' alt=''/>
+        </a>
+        <Menu>
+          <a href='/'>Model 3</a>
+          <a href='/'>Model Y</a>
+          <a href='/'>Model S</a>
+          <a href='/'>Model X</a>
+        </Menu>
+        <RightMenu>
+          <a href='/'>Shop</a>
+          <a href='/'>Tesla Account</a>
+          <CustomMenu onClick={() => setBurgerStatus(true)}/>
+        </RightMenu>
+        <BurgerNav show={burgerStatus} onClick={() => setBurgerStatus(false)}>
+          <CloseWrapper>
+            <CustomClose/>
+          </CloseWrapper>
+          <li><a href="/">Existing Inventory</a></li>
+          <li><a href="/">Used Inventory</a></li>
+          <li><a href="/">Trade-in</a></li>
+          <li><a href="/">Cybertruck</a></li>
+          <li><a href="/">Roadaster</a></li>
+          <li><a href="/">Semi</a></li>
+          <li><a href="/">Existing Inventory</a></li>
+          <li><a href="/">Existing Inventory</a></li>
+        </BurgerNav>
+    </Container>
   )
 }
 
